a5e49ccccb34d48a3e9e18dff577305c9283800f
Hemanth <80045609+7989675145@users.noreply.github.com>
2022-04-30 12:01:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{createContext,useState} from 'react'
+import ComponentA from './ComponentA';
+import ComponentB from './ComponentB';
 
-function App() {
+export const store = createContext();
+const App = () => {
+  const [data,setData] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <store.Provider value={[data,setData]}> 
+       <center>
+        <ComponentA />
+        <ComponentB />
+        <button onClick={()=>setData(data+1)}>Increment</button>
+      </center>
+     </store.Provider>
+    )
 }
 
-export default App;
+export default App
